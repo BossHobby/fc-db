a694c64d69e33c0e3ca2a72568df153c1261b458
bkleiner <b.kleiner@str8labs.com>
2022-08-27 18:23:24 +0200

    update

---
+++ target/NERC-FLOWBOXV2/target.h
@@ -0,0 +1,56 @@
+
+#include "config.h"
+#include "config_helper.h"
+
+#define FLOWBOXV2
+
+//PORTS
+#define SPI_PORTS \
+  SPI1_PA5PA6PA7 \
+  SPI3_PB3PB4PB5
+
+#define USART_PORTS \
+	USART1_PA10PA9 \
+	USART2_PA3PA2
+
+//LEDS
+#define LED_NUMBER 1
+#define LED1PIN PIN_C13
+#define LED1_INVERT
+
+#define BUZZER_PIN PIN_B8
+
+//GYRO
+#define GYRO_SPI_PORT SPI_PORT1
+#define GYRO_NSS PIN_A4
+#define GYRO_INT PIN_A1
+#define GYRO_ORIENTATION GYRO_ROTATE_90_CCW
+
+//RADIO
+
+
+#ifdef SERIAL_RX
+#define RX_USART USART_PORT2
+#endif
+
+
+
+
+
+#define USE_SDCARD
+#define SDCARD_SPI_PORT SPI_PORT3
+#define SDCARD_NSS_PIN PIN_A15
+
+
+
+
+
+// MOTOR PINS
+//S3_OUT
+#define MOTOR_PIN0 MOTOR_PIN_PB1
+//S4_OUT
+#define MOTOR_PIN1 MOTOR_PIN_PB10
+//S1_OUT
+#define MOTOR_PIN2 MOTOR_PIN_PA0
+//S2_OUT
+#define MOTOR_PIN3 MOTOR_PIN_PB0
